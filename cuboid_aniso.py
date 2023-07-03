870cd5ce5887e9df3ac76c9caf743d014e3bea3b
kevinkk <gzk@lsc.cc.ac.cn>
2023-07-03 09:44:25 +0800

    Modify cuboid_aniso.py and add two meshes

--- cuboid_aniso.py
+++ cuboid_aniso.py
@@ -16,23 +16,24 @@ v_b = [0.0, 0.0, 0.0]
 len_dx = 1.0
 len_dy = 1.0
 len_dz = 1.0
+rb = False
 
 # n_dx nd_y nd_z 每层的划分数
 # h_dx h_dy h_dz 每层的高度（累计高度，归一化）
-
-# n_dx = [1, 1]
-# h_dx = [0.4, 1.0]
-# n_dy = [1, 1]
-# h_dy = [0.6, 1.0]
-# n_dz = [1, 1, 1]
-# h_dz = [0.4, 0.6, 1.0]
 
 n_dx = [1, 1]
 h_dx = [0.5, 1.0]
 n_dy = [1, 1]
 h_dy = [0.5, 1.0]
-n_dz = [1, 1]
-h_dz = [0.5, 1.0]
+n_dz = [1, 1, 1]
+h_dz = [0.3, 0.7, 1.0]
+
+# n_dx = [1, 1]
+# h_dx = [0.5, 1.0]
+# n_dy = [1, 1]
+# h_dy = [0.5, 1.0]
+# n_dz = [1, 1]
+# h_dz = [0.5, 1.0]
 
 # We first create a cuboid:
 # rec = gmsh.model.occ.addRectangle(v_b[0], v_b[1], v_b[2], lens[0], lens[1])
@@ -47,10 +48,10 @@ v = gmsh.model.occ.add_point(v_b[0], v_b[1], v_b[2])
 dimtags = gmsh.model.occ.extrude([(0, v)], len_dx, 0, 0, n_dx, h_dx) # heights 层高（百分比，累计高度）
 dt_l.append(dimtags)
 # print(dimtags)
-dimtags = gmsh.model.occ.extrude([(1, dimtags[1][1])], 0, len_dy, 0, n_dy, h_dy, recombine=True) # heights 层高（百分比，累计高度）
+dimtags = gmsh.model.occ.extrude([(1, dimtags[1][1])], 0, len_dy, 0, n_dy, h_dy, recombine=rb) # heights 层高（百分比，累计高度）
 dt_l.append(dimtags)
 # print(dimtags)
-dimtags = gmsh.model.occ.extrude([(2, dimtags[1][1])], 0, 0, len_dz, n_dz, h_dz, recombine=True) # heights 层高（百分比，累计高度）
+dimtags = gmsh.model.occ.extrude([(2, dimtags[1][1])], 0, 0, len_dz, n_dz, h_dz, recombine=rb) # heights 层高（百分比，累计高度）
 dt_l.append(dimtags)
 print(dimtags)
 
@@ -59,7 +60,8 @@ gmsh.model.occ.synchronize()
 
 gmsh.model.mesh.generate(3)
 # gmsh.write("cuboid_aniso.mesh")
-gmsh.write("cuboid_aniso2.mesh")
+# gmsh.write("cuboid_aniso2.mesh")
+gmsh.write("cuboid_aniso3_tet.mesh")
 
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
